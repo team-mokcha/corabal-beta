b0c6e63a9c29cd18c08fc71c319b2536559d384a
HyejinYang <myosotis-0@naver.com>
2021-11-26 19:57:58 +0900

    [#38] refactor: Divide record component to template

--- src/pages/record/template/index.tsx
+++ src/pages/record/template/index.tsx
@@ -1,1 +1,26 @@
-// 분리 필요
+import React, { ReactElement } from "react";
+import { View, Image } from "react-native";
+import { ButtonGradient, Header } from "@Components";
+import { DateText } from "../../../components/atoms/index";
+import { PickingContainer } from "../../../components/organisms/index";
+import styles from "./record.style";
+
+const Template = (): ReactElement => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Header back={false} close={true} />
+        <DateText type="present" />
+        <Image style={styles.cupImage} source={require("@assets/cup-of-today.png")} />
+        {/* total option container */}
+        <View style={styles.totalPickingContainer}>
+          <PickingContainer />
+        </View>
+        {/* 11월 셋째 주 스프린트 [11/5 - 11/21] #44 댓글 참고 - 21.11.18 수연 */}
+        <ButtonGradient style={{ marginBottom: "auto", maxHeight: 48 }} title="완료" />
+      </View>
+    </>
+  );
+};
+
+export default Template;
