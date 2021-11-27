e8ed9c2c34fdbacf562224a63e5d4d9e93618932
HyejinYang <myosotis-0@naver.com>
2021-11-27 11:45:26 +0900

    [#14] feat: Add alert & Fix error handling logic of record

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React, { ReactElement } from "react";
 import { AppBootstrap } from "@Components";
 import Navigator from "./pages/navigator/index";
 import { LogBox } from "react-native";
+import Toast from "react-native-toast-message";
 
 // LogBox.ignoreLogs(["Setting a timer"]);
 
 export default function App(): ReactElement {
   return (
-    <AppBootstrap>
-      <Navigator />
-    </AppBootstrap>
+    <>
+      <AppBootstrap>
+        <Navigator />
+      </AppBootstrap>
+      <Toast />
+    </>
   );
 }
--- src/pages/record/record.tsx
+++ src/pages/record/record.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "../navigator/index";
 import { useState as HSUseState } from "@hookstate/core";
 import { globalUserState, globalCoffeePickState } from "@stores/stores";
 import { addNormalCupLog } from "@services/functions/handle-coffee-log";
+import Toast from "react-native-toast-message";
 import Template from "./template/index";
 
 type NavigationProps = {
@@ -11,9 +12,10 @@ type NavigationProps = {
 };
 
 const Record = ({ navigation }: NavigationProps): ReactElement => {
-  // 2021.11.26 - 아래 unsubscribe는 수연이 리팩 시 살펴보고 바꾸기로.
+  // 2021.11.26 - 아래 unsubscribe는 수연이 리팩 시 살펴보고 바꾸기로
   // useEffect(() => {
   //   const unsubscribe = navigation.addListener("focus", () => {
+  //     //
   //   });
   //   return unsubscribe;
   // }, []);
@@ -33,9 +35,6 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
     pickedOption = [];
   }
 
-  // 데이터 추가하는 함수 작성 후, props로 넘겨주기
-  // 1) email, shot, base, option(처리 필요), timestamp 생성
-  // 2) addNomalCupRecord 불러온 후 에러 처리 > 알럿 띄워주기
   const handleAddNormalCupBtn = async () => {
     const timestamp = new Date();
     const [result, contents] = await addNormalCupLog(
@@ -46,17 +45,26 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
       timestamp
     );
 
-    switch (result) {
-      case "succeed":
-        {
-          console.log("Data was added successfully", contents);
-        }
-        break;
-      case "failed":
-        {
-          console.log("An error occurred", contents);
-        }
-        break;
+    try {
+      if (result === "succeed") {
+        Toast.show({
+          type: "info",
+          text1: "커피 기록 저장 완료!"
+        });
+        navigation.navigate("Index"); // 네비게이션 미작동 원인 알아보기
+      } else if (result === "failed") {
+        Toast.show({
+          type: "error",
+          text1: "서버와의 통신 실패",
+          text2: `${contents}로 인해 서버와의 통신이 실패했습니다. 다시 시도해주세요.`
+        });
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "알 수 없는 오류",
+        text2: `${error}로 인해 오류가 발생했습니다. 다시 시도해주세요.`
+      });
     }
   };
 
