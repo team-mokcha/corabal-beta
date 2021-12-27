bc9bb03f31df1858ec01dd4d65d4abad4ed38fab
HyejinYang <myosotis-0@naver.com>
2021-12-28 01:58:37 +0900

    [#14] chore : Delete unnecessary files

--- src/components/molecules/record/pickedRow.tsx
+++ src/components/molecules/record/pickedRow.tsx
@@ -1,13 +1,6 @@
 import React, { ReactElement } from "react";
 import { View } from "react-native";
-import {
-  Text,
-  CupIcon,
-  DeleteIcon,
-  PressedShotIcon,
-  PressedBaseIcon,
-  PressedOptionIcon
-} from "../../atoms/index";
+import { Text, CupIcon, DeleteIcon } from "../../atoms/index";
 import styles from "./pickedRow.style";
 
 const ROW_TEXT = {
@@ -25,66 +18,66 @@ type PickingRowProps = {
 };
 
 const PickedRow = ({ shot, base, option }: PickingRowProps): ReactElement => {
-  const ShotIcon = () => {
-    switch (shot) {
-      case 1:
-        return <PressedShotIcon type="onePressed" imageStyle={styles.pickedIcon} />;
-      case 2:
-        return <PressedShotIcon type="twoPressed" imageStyle={styles.pickedIcon} />;
-      case 3:
-        return <PressedShotIcon type="threePressed" imageStyle={styles.pickedIcon} />;
-    }
-  };
+  // const ShotIcon = () => {
+  //   switch (shot) {
+  //     case 1:
+  //       return <PressedShotIcon type="onePressed" imageStyle={styles.pickedIcon} />;
+  //     case 2:
+  //       return <PressedShotIcon type="twoPressed" imageStyle={styles.pickedIcon} />;
+  //     case 3:
+  //       return <PressedShotIcon type="threePressed" imageStyle={styles.pickedIcon} />;
+  //   }
+  // };
 
-  const BaseIcon = () => {
-    switch (base) {
-      case "water":
-        return (
-          <>
-            <PressedBaseIcon type="milkPressed" imageStyle={styles.pickedIcon} />
-            <Text>{ROW_TEXT.water}</Text>
-          </>
-        );
-      case "milk":
-        return (
-          <>
-            <PressedBaseIcon type="waterPressed" imageStyle={styles.pickedIcon} />
-            <Text>{ROW_TEXT.milk}</Text>
-          </>
-        );
-    }
-  };
+  // const BaseIcon = () => {
+  //   switch (base) {
+  //     case "water":
+  //       return (
+  //         <>
+  //           <PressedBaseIcon type="milkPressed" imageStyle={styles.pickedIcon} />
+  //           <Text>{ROW_TEXT.water}</Text>
+  //         </>
+  //       );
+  //     case "milk":
+  //       return (
+  //         <>
+  //           <PressedBaseIcon type="waterPressed" imageStyle={styles.pickedIcon} />
+  //           <Text>{ROW_TEXT.milk}</Text>
+  //         </>
+  //       );
+  //   }
+  // };
 
-  const OptionIcon = () => {
-    switch (option) {
-      case "syrup":
-        return (
-          <>
-            <PressedOptionIcon type="syrupPressed" imageStyle={styles.pickedIcon} />
-            <Text>{ROW_TEXT.syrup}</Text>
-          </>
-        );
-      case "cream":
-        return (
-          <>
-            <PressedOptionIcon type="creamPressed" imageStyle={styles.pickedIcon} />
-            <Text>{ROW_TEXT.cream}</Text>
-          </>
-        );
-      case "none":
-        return null;
-    }
-  };
+  // const OptionIcon = () => {
+  //   switch (option) {
+  //     case "syrup":
+  //       return (
+  //         <>
+  //           <PressedOptionIcon type="syrupPressed" imageStyle={styles.pickedIcon} />
+  //           <Text>{ROW_TEXT.syrup}</Text>
+  //         </>
+  //       );
+  //     case "cream":
+  //       return (
+  //         <>
+  //           <PressedOptionIcon type="creamPressed" imageStyle={styles.pickedIcon} />
+  //           <Text>{ROW_TEXT.cream}</Text>
+  //         </>
+  //       );
+  //     case "none":
+  //       return null;
+  //   }
+  // };
 
   return (
     <View>
       <CupIcon imageStyle={styles.cupIcon} />
-      <ShotIcon />
+      {/* <ShotIcon /> */}
       <Text>
         {shot} {ROW_TEXT.shot}
       </Text>
-      <BaseIcon />
-      <OptionIcon />
+      {/* <BaseIcon /> */}
+      {/* <OptionIcon /> */}
       <DeleteIcon />
     </View>
   );
