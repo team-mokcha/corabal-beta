5a25c2a6d2369159d7a84d170284e934edf2721d
HyejinYang <myosotis-0@naver.com>
2022-01-04 17:31:37 +0900

    [#57] chore: Add cup images & Delete unneccessary parts of shopbody

--- src/components/organisms/shop/shopBody.style.ts
+++ src/components/organisms/shop/shopBody.style.ts
@@ -1,15 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  cupWrapper: {
-    alignItems: "center",
-    marginVertical: 40,
-    marginHorizontal: 24
-  },
   wrapper: {
     flexDirection: "row",
     justifyContent: "space-between",
-    margin: 24
+    marginHorizontal: 24,
+    marginVertical: 40
+  },
+  cupWrapper: {
+    alignItems: "center"
   },
   point: {
     width: 12,
@@ -18,24 +17,6 @@ const styles = StyleSheet.create({
   emptyCup: {
     width: 72,
     height: 72
-  },
-  buyingCup: {
-    width: 41,
-    height: 76,
-    marginTop: 56,
-    marginBottom: 13
-  },
-  pointFont: {
-    fontSize: 13
-  },
-  myPointFont: {
-    fontSize: 12,
-    marginTop: -9
-  },
-  modalButton: {
-    width: 136,
-    height: 48,
-    margin: -10
   }
 });
 
--- src/components/organisms/shop/shopBody.tsx
+++ src/components/organisms/shop/shopBody.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from "react";
 import { TouchableOpacity, View, Image } from "react-native";
-import { Text } from "@Components";
+import Point from "../../atoms/icons/point";
 import styles from "./shopBody.style";
+
+// 컵 가게의 컵은 고정되어 있음. 이미지만 달라짐. 통신해서 불러오는 것도 아님.
+// 단, 보유 중  여부가 문제.
+// 일단....
 
 const ShopBody = (): ReactElement => {
   return (
@@ -12,32 +16,7 @@ const ShopBody = (): ReactElement => {
         style={styles.cupWrapper}
       >
         <Image style={styles.emptyCup} source={require("@assets/empty-cup.png")} />
-        <Text>
-          <Image style={styles.point} source={require("@assets/btn_point.png")} />
-          <Text>{} 30p</Text>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        // onPress={() => setWantToBuy(!wantToBuy)}
-        activeOpacity={0.5}
-        style={styles.cupWrapper}
-      >
-        <Image style={styles.emptyCup} source={require("@assets/empty-cup.png")} />
-        <Text>
-          <Image style={styles.point} source={require("@assets/btn_point.png")} />
-          <Text>{} 100p</Text>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        // onPress={() => setWantToBuy(!wantToBuy)}
-        activeOpacity={0.5}
-        style={styles.cupWrapper}
-      >
-        <Image style={styles.emptyCup} source={require("@assets/empty-cup.png")} />
-        <Text>
-          <Image style={styles.point} source={require("@assets/btn_point.png")} />
-          <Text>{} 100p</Text>
-        </Text>
+        <Point points={30} fontSize="large" style={{ marginTop: 10 }} />
       </TouchableOpacity>
     </View>
   );
